f38dbd8953635f83056ea9eed84e2c03cf60fe58
Geoffrey Blech <gdblech@uncg.edu>
2017-10-23 17:36:52 -0400

    bunch of stuff

---
+++ User/src/Profile.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class Profile {
+    /* private attributes:
+    firstName: string
+    lastName: string
+    location: object ( city, state, country)
+    email: string
+    reviews: linked list
+    username : string
+    */
+    String firstName;
+    String lastName;
+    String account;
+    String email;
+    LinkedList<Review> reviews;
+    Location location;
+
+}
+
+class Location{
+    String city;
+    String state;
+    String country;
+
+}
---
+++ User/src/UserUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class UserUI {
+    private JPanel f;
+    private JButton signInButton;
+    private JTextField searchTF;
+    private JButton searchButton;
+    private JScrollPane results;
+}
